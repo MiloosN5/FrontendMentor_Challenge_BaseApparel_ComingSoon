d536acdf2e4f2c8b6919f3f2975c9f20e27d9a50
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2023-11-14 14:13:03 +0100

    adding
    adding visually hidden class for the screen readers

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,6 +4,7 @@ import heroDesktop from '../assets/hero-desktop.jpg'
 const Image = () => {
   return (
     <section className="image">
+      <h2 className='visually-hidden'>Hero image</h2>
       <picture>
         <source srcSet={heroDesktop} media="(min-width: 48.625em)" />
         <img src={heroMobile} alt="..." />
